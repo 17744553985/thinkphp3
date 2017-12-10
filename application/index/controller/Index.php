eef2069bf9242de79c006aca0735bda8fd8d9542
zl910514 <17744553985@163.com>
2017-12-10 16:33:15 +0800

    thinkphp5

---
+++ application/index/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+class Index extends Controller
+{
+    public function index()
+    {
+        return '1111111111111111111111111';
+    }
+
+
+    public function text(){
+
+    	return '111111111111';
+    }
+}
---
+++ application/index/controller/Text.php
@@ -0,0 +1,23 @@
+<?php 
+namespace app\index\controller;
+use think\Controller;
+class Text extends Controller{
+
+     public function index(){
+
+     	return 11111;
+   }
+     public function xiix(){
+    
+       
+
+       return view('xiix',['name'=>'thinqqquikphp']);
+     }
+}
+
+
+
+
+
+
+
